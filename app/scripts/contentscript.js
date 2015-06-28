91f978b607914ac0c2b01ad514495a78309e63bf
Koki Takahashi <hakatasiloving@gmail.com>
2015-06-28 20:40:37 +0900

    CI happy

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,13 +1,13 @@
 'use strict';
 
+// Run SL, run!
+function runSL(options) {
+  return options;
+}
+
 // Listen for SL request from omnibox
 chrome.runtime.onMessage.addListener(function (request) {
   if (request.mode === 'run') {
     runSL(request.options);
   }
 });
-
-// Run SL, run!
-function runSL(options) {
-  return options;
-}
